a834305c86a6956e94886aa933c337d3c8a73d08
Tech-A65 <tech-a65@Tech-A56s-MacBook-Air.local>
2018-10-26 09:14:30 +0100

    Updated Readme

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,24 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+apt_update 'update' do
+  action :update
+end
+
+apt_repository 'mongodb-org' do
+  uri "http://repo.mongodb.org/apt/ubuntu"
+  distribution "xenial/mongodb-org/3.2"
+  components ['multiverse']
+  keyserver "hkp://keyserver.ubuntu.com:80"
+  key "EA312927"
+  action :add
+end
+
+package 'mongodb-org' do
+   action :upgrade
+end
+
+service 'mongod' do
+  supports status: true, restart: true, reload: true
+  action [:enable, :start]
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -11,25 +11,29 @@ describe 'mongoDB::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it 'should install mongodb' do
+      expect(chef_run).to upgrade_package 'mongodb-org'
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should add mongo to the source list' do
+      expect(chef_run).to add_apt_repository('mongodb-org')
     end
+
+    it 'should update all sources' do
+      expect(chef_run).to update_apt_update('update')
+    end
+
+    it 'enables the mongod service' do
+      expect(chef_run).to enable_service 'mongod'
+    end
+
+    it 'starts the mongodb service' do
+      expect(chef_run).to start_service 'mongod'
+    end
+
   end
 end
